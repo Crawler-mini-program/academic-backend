918035fdad97c08cc2d1f301109cebd057fc8620
mike-egg123 <qrn20000312@163.com>
2021-03-07 17:49:46 +0800

    新增教师的关注和取消关注接口

--- src/main/java/com/crawler/backend/controller/TeacherList.java
+++ src/main/java/com/crawler/backend/controller/TeacherList.java
@@ -4,6 +4,7 @@ import net.sf.json.JSONArray;
 import com.crawler.backend.service.TeacherListService;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import net.sf.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +16,41 @@ import java.util.List;
 @AllArgsConstructor
 public class TeacherList {
     private TeacherListService teacherListService;
-    @GetMapping("/get_teacher_list")
+    @GetMapping("/get-teacher-list")
     @ApiOperation("获取关注的老师列表")
     public JSONArray get_teachers(HttpServletRequest request){
-        String uid = request.getAttribute("openId").toString();
-        List<com.crawler.backend.model.TeacherList> teachers = teacherListService.getTeacher(uid);
+        //先注释掉，使用测试中的uid
+//        String uid = request.getAttribute("openId").toString();
+        List<com.crawler.backend.model.TeacherList> teachers = teacherListService.getTeacher("oVhiP5cyPkaR-Gc2FG7e76WCEdrI");
         JSONArray json = JSONArray.fromObject(teachers);
         return json;
+    }
+    @GetMapping("/follow-teacher")
+    @ApiOperation("关注老师")
+    public JSONObject follow_teacher(HttpServletRequest request, String name, String school, long tid){
+//        String uid = request.getAttribute("openId").toString();
+        com.crawler.backend.model.TeacherList teacherList = new com.crawler.backend.model.TeacherList(name, school, "oVhiP5cyPkaR-Gc2FG7e76WCEdrI", tid);
+        teacherListService.addTeacher(teacherList);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.accumulate("code", "0");
+        jsonObject.accumulate("msg", "关注成功");
+        return jsonObject;
+    }
+    @GetMapping("/defollow-teacher")
+    @ApiOperation("取消关注老师")
+    public JSONObject defollow_teacher(HttpServletRequest request, long tid){
+//        String uid = request.getAttribute("openId").toString();
+//        com.crawler.backend.model.TeacherList teacherList = new com.crawler.backend.model.TeacherList(name, school, "oVhiP5cyPkaR-Gc2FG7e76WCEdrI", tid);
+        JSONObject jsonObject = new JSONObject();
+        if(teacherListService.deleteTeacher(tid, "oVhiP5cyPkaR-Gc2FG7e76WCEdrI")){
+            jsonObject.accumulate("code", "0");
+            jsonObject.accumulate("msg", "取消关注成功");
+
+        }
+        else{
+            jsonObject.accumulate("code", "1");
+            jsonObject.accumulate("msg", "该tid不存在");
+        }
+        return jsonObject;
     }
 }
